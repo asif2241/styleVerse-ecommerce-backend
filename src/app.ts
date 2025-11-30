0ca549cc5c2dacf65bb8bb56a07129ae1098d18b
asif2241 <asifislam2241@gmail.com>
2025-11-30 10:05:50 +0600

    project initialized

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import cookieParser from "cookie-parser"
+import express, { Request, Response, urlencoded } from "express"
+import expressSession from "express-session"
+import passport from "passport"
+import cors from "cors"
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler"
+import notFound from "./app/middlewares/notFound"
+import { envVars } from "./app/config/env"
+// import "./app/config/passport"
+import { router } from "./app/routes"
+
+const app = express()
+
+// app.use(expressSession({
+//     secret: envVars.EXPRESS_SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false
+// }))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(cookieParser())
+app.use(express.json())
+app.set("trust proxy", 1);
+app.use(urlencoded({ extended: true }))
+app.use(cors({
+    origin: ["http://localhost:3000", "https://parcel-delivery-client-b5a6.vercel.app"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}))
+
+
+
+app.use("/api/v1", router)
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Welcome to Parcel Delivery System Backend"
+    })
+})
+
+app.use(globalErrorHandler)
+app.use(notFound)
+
+export default app
---
+++ src/app/config/env.ts
@@ -0,0 +1,82 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface EnvConfig {
+    PORT: string;
+    DB_URL: string;
+    NODE_ENV: "development" | "production",
+    JWT_ACCESS_SECRET: string,
+    JWT_ACCESS_EXPIRES: string,
+    JWT_REFRESH_SECRET: string,
+    JWT_REFRESH_EXPIRES: string,
+    BCRYPT_SALT_ROUND: string,
+    // GOOGLE_CLIENT_SECRET: string,
+    // GOOGLE_CLIENT_ID: string,
+    // GOOGLE_CALLBACK_URL: string,
+    // EXPRESS_SESSION_SECRET: string,
+    // FRONTEND_URL: string,
+    SUPER_ADMIN_EMAIL: string,
+    SUPER_ADMIN_PASSWORD: string,
+    // CLOUDINARY_CLOUD_NAME: string,
+    // CLOUDINARY_API_KEY: string,
+    // CLOUDINARY_API_SECRET: string,
+    // EMAIL_SENDER: {
+    //     SMTP_HOST: string,
+    //     SMTP_PASS: string,
+    //     SMTP_PORT: string,
+    //     SMTP_USER: string,
+    //     SMTP_FROM: string
+    // },
+    // REDIS_HOST: string,
+    // REDIS_PORT: string,
+    // REDIS_USERNAME: string,
+    // REDIS_PASSWORD: string
+
+
+}
+
+const loadEnvVariables = (): EnvConfig => {
+    const requiredVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES", "BCRYPT_SALT_ROUND", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD"]
+
+    requiredVariables.forEach(key => {
+        if (!process.env[key]) {
+            throw new Error(`Missing require environment variable ${key}`)
+        }
+    })
+
+    return {
+        PORT: process.env.PORT as string,
+        DB_URL: process.env.DB_URL as string,
+        NODE_ENV: process.env.NODE_ENV as "development" | "production",
+        JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+        JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+        JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+        JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
+        BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+        // GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+        // GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+        // GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
+        // EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
+        // FRONTEND_URL: process.env.FRONTEND_URL as string,
+        SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
+        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
+        // CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+        // CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+        // CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
+        // EMAIL_SENDER: {
+        //     SMTP_HOST: process.env.SMTP_HOST as string,
+        //     SMTP_PASS: process.env.SMTP_PASS as string,
+        //     SMTP_PORT: process.env.SMTP_PORTE as string,
+        //     SMTP_USER: process.env.SMTP_USER as string,
+        //     SMTP_FROM: process.env.SMTP_FROM as string
+        // },
+        // REDIS_HOST: process.env.REDIS_HOST as string,
+        // REDIS_PORT: process.env.REDIS_PORT as string,
+        // REDIS_USERNAME: process.env.REDIS_USERNAME as string,
+        // REDIS_PASSWORD: process.env.REDIS_PASSWORD as string,
+
+    }
+}
+
+export const envVars = loadEnvVariables()
